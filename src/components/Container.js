59a1aca0464b55d1f9c7f6cb7bda9d13bac7fbe6
Simon Tinsley <45486136+sjtinsley@users.noreply.github.com>
2022-07-07 15:11:18 +0100

    Container on page with a header for a field

---
+++ src/components/Container.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { FieldHeader } from './FieldHeader';
+
+export const Container = (props) => {
+  return (
+    <div className="direction-container"><FieldHeader title="Origin" /></div>
+  )
+}
---
+++ src/components/FieldHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const FieldHeader = (props) => {
+  return (
+    <h1>{props.title}</h1>
+  )
+}
