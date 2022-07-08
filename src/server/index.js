6feae3257f099eb0ac2caae7b1f2674d0e6cb0c6
Tom O'Neill <tomoneill32@gmail.com>
2022-07-08 11:27:54 +0100

    Removing unneeded code

--- src/server/index.js
+++ src/server/index.js
@@ -2,22 +2,10 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const serve = require('koa-static');
 const callHereApi = require('../services/here_API');
-const bodyParser = require('koa-bodyparser');
-const cors = require('@koa/cors');
-
 
 const app = new Koa();
 const router = new Router();
 
-router.post('/getevroute', async (ctx, next) => {
-  console.log('HELLLLOOOOOO');
-  callApi(ctx.request.params.origin,ctx.request.params.destination)
-  await next();
-  console.log(ctx.body)
-});
-
-app.use(cors());
-app.use(bodyParser());
 app.use(serve('dist'));
 app.use(router.routes());
 app.use(router.allowedMethods());
