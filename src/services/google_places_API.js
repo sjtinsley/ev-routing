ece05359a2bd28b5b3c48c7cc216f8ff17db8cda
timbo-graham <tmgraham29@gmail.com>
2022-07-11 10:58:36 +0100

    Google Places call working: it returns a list of POIs when given multiple waypoints as returned by the HERE API

---
+++ src/services/google_places_API.js
@@ -0,0 +1,32 @@
+const env = require("../../.env")
+
+const pois = []
+
+const getPois = async (callHereOutput) => {
+  
+  const waypoints = callHereOutput.split(`%3B`);
+  const urls = waypoints.map((waypoint) => {
+    return `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${env.google_api_key}&location=${waypoint}&radius=1500`
+  })
+
+  Promise.all(urls.map(url =>
+    fetch(url).then(response => {
+      return response.json()
+    })
+    )).then(data => {
+      data.forEach((place) => {
+        let destinations = []
+        place.results.forEach((poi) => {
+          destinations.push({
+            name: poi.name,
+            location: poi.geometry.location,
+            rating: poi.rating
+          })
+          pois.push(destinations)
+        })
+      });
+      console.log(pois);
+  })
+}
+
+getPois("51.80636635113677%2C-0.3465737695457438%3B51.82465490329337%2C-0.3675335170844881");
